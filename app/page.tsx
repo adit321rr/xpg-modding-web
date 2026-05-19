11c054822779fe31a714970f107aad417f30f14b
Nasyrun Adetiya <nasyrunadetiya1@gmail.com>
2026-05-19 07:10:40 +0700

    false

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 export const revalidate = 0;
 
 export default async function Home() {
-  const isMaintenance = true;
+  const isMaintenance = false;
 
   if (isMaintenance) {
     return (
